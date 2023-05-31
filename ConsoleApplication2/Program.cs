58d5da9328c2f4a49002469fa7d26cac82ce37dd
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-05-31 23:25:32 +0200

    Integers

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            //integers
+            int variable_1 = Int32.MinValue + 100;
+            Console.WriteLine(variable_1);
+
+            uint variable_uint = uint.MaxValue;
+            Console.WriteLine(variable_uint);
+            
+            var variable_2 = "Hello";
+            Console.WriteLine(variable_2);
+
+            byte variable_3 = Byte.MaxValue;
+            Console.WriteLine(variable_3);
+
+            sbyte variable_sbyte = SByte.MinValue;
+            Console.WriteLine(variable_sbyte);
+            
+            //
+
+            short variable_4 = short.MaxValue;
+            Console.WriteLine(variable_4);
+
+            ushort variable_5 = ushort.MaxValue;
+            Console.WriteLine(variable_5);
+
+            //
+            
+            long variable_long = long.MaxValue;
+            Console.WriteLine(variable_long);
+
+            ulong variable_ulong = ulong.MaxValue;
+            Console.WriteLine(variable_ulong);
+            
+            //
+
+            Console.ReadKey();
+        }
+    }
+}
