6a117461c45431ef72d2a4b4cc389e832552398f
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-04 20:57:06 +0200

    Loops

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WHILE | FOR | DO WHILE
+
+
+            //WHILE
+            int a = 1;
+
+            while (a <= 5)
+            {
+                Console.WriteLine("Text " + a);
+                a++;
+            }
+
+            Console.WriteLine();
+
+            //FOR
+            int b = 1;
+
+            for ( ; b <= 5 ; b++)
+            {
+                Console.WriteLine("Text " + b);
+            }
+
+            Console.WriteLine();
+
+            //DO WHILE
+
+            do
+            {
+                Console.WriteLine("Text");
+            }
+            while (false);
+
+            Console.ReadKey();
+        }
+    }
+}
