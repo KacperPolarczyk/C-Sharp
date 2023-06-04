aae48e492aab21a9e54865d1a07271810bfc4124
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-04 20:20:41 +0200

    Statements - if, else if, else

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+
+            if (a > 0 && a > 9)
+            {
+                Console.WriteLine("The number A is greater than 0 and 9.");
+            }
+            else if (a == 9)
+            {
+                Console.WriteLine("The number A is equal to 9.");
+            }
+            else if(a > 0) 
+            {
+                Console.WriteLine("The number A is greater than 0.");
+            }
+            else if(a == 0) 
+            {
+                Console.WriteLine("The number A is equal to 0.");
+            }
+            else
+            {
+                Console.WriteLine("The number A is less than 0.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
