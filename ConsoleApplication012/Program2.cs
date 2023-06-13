ef60533fabf4cb5947e39481b60046d39e3d09ca
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-13 22:24:31 +0200

    Functions / Methods

---
+++ ConsoleApplication012/Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            Math("Result: ", a, b, d:2);
+            Math("Result: ", 4, 3, d: 3);
+            Math("Result: ", 23, 312, d:4);
+            Console.ReadKey();
+        }
+
+        static void Math(string text, int a, int b, int c = 2, int d = 0)
+        {
+            int result = a + b + c + d;
+            Console.WriteLine(text + result);
+        }
+    }
+}
