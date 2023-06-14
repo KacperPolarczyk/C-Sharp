efa913a24286ec422d3cc84cf0c74e0d499be0bd
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-14 21:18:44 +0200

    Functions / Methods

---
+++ ConsoleApplication012/Program5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[2];
+            array[0] = 3;
+            array[1] = 4;
+
+            Console.WriteLine("Before: " + array[0] + " , " + array[1]);
+            Console.WriteLine();
+
+            Test(array);
+
+            Console.WriteLine("After: " + array[0] + " , " + array[1]);
+            Console.ReadKey();
+        }
+
+        static void Test(int[] a)
+        {
+            a[0] = 333;
+            a[1] = 444;
+
+            Console.WriteLine("Function: " + a[0] + " , " + a[1]);
+        }
+    }
+}
