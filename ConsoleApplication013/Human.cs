eed851f9ac041afb95b4bf26f7d13a4e098c47e6
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-15 20:41:10 +0200

    Classes and Objects (Part 1)

---
+++ ConsoleApplication013/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Human
+    {
+        public string name = "Steve";
+        public string surname = "Smith";
+
+        public void Text()
+        {
+            Console.WriteLine("Hello! I'm " + name + " " + surname);
+        }
+    }
+}
---
+++ ConsoleApplication013/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human new_person = new Human();
+            Console.WriteLine(new_person.name);
+
+            Human new_person2 = new Human();
+            new_person2.name = "John";
+            Console.WriteLine(new_person2.name);
+
+            new_person.Text();
+
+            new_person2.Text();
+
+            Console.ReadKey();
+        }
+    }
+}
