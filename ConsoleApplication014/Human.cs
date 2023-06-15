8b56ff3084ea69a8e097b42373768aa20b2d6d26
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-15 22:49:43 +0200

    Classes and Objects (Part 2)
    Constructors & Destructors

---
+++ ConsoleApplication014/Human.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Human
+    {
+        //Constructor
+
+        public Human()
+        {
+
+        }
+
+        public Human(string nameConstructor, string surnameConstructor)
+        {
+            //Console.WriteLine("Constructor");
+            name = nameConstructor;
+            surname = surnameConstructor;
+        }
+
+
+        public string name = "No name";
+        public string surname = "No name";
+
+        public void Text()
+        {
+            Console.WriteLine(name + " " + surname);
+        }
+
+        //Destructor
+        ~Human()
+        {
+            System.Diagnostics.Trace.WriteLine("Destructor");
+            Console.WriteLine("Destructor");
+        }
+
+    }
+}
+
---
+++ ConsoleApplication014/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human new_person = new Human("John", "Smith");
+
+            new_person.Text();
+
+            new_person.name = "Oscar";
+            new_person.Text();
+
+
+            Console.ReadKey();
+        }
+    }
+}
