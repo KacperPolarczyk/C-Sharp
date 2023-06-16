675a8928e963dec423f671d95ad7aaf86e4a0b78
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-16 21:12:45 +0200

    Properties - Getter&Setter

---
+++ ConsoleApplication016/Human.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    internal class Human
+    {
+        public string name;
+
+        //private int age;
+
+        //public int age
+        //{ 
+        //    get; 
+        //    set;
+        //}
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            { 
+                if(value >= 0 && value <= 200)
+                {
+                    age = value;
+                }
+                else
+                    age = 0;
+            }
+        }
+
+        //public int getAge()
+        //{
+        //    return age;
+        //}
+
+        //public int setAge(int human_age)
+        //{
+        //    if (age >= 0 && age <= 200)
+        //    {
+        //        return age = human_age;
+        //    }
+        //    else
+        //        return 0;
+
+        //}
+    }
+}
---
+++ ConsoleApplication016/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human kacper = new Human();
+            kacper.name = "Kacper";
+            //kacper.setAge(19);
+
+            kacper.Age = 19;
+            //kacper.Age = -19;
+
+            //Console.WriteLine($"I'm {kacper.name}, my age is {kacper.getAge()}.");
+            Console.WriteLine($"I'm {kacper.name}, my age is {kacper.Age}.");
+
+            Console.ReadKey();
+        }
+    }
+}
