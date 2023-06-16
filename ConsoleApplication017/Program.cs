ff48d9062077f54f82f9a214e78275d6c36f0416
MasterCode <95491287+KacperPolarczyk@users.noreply.github.com>
2023-06-16 21:43:36 +0200

    Strings - params and object

---
+++ ConsoleApplication017/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object obj = 1;
+            //object obj2 = "Text";
+
+            Console.WriteLine(TextGenerator("I'm #0 and I'am #1 years old.", "Kacper", 19));
+
+            Console.ReadKey();
+        }
+
+        public static string TextGenerator(string text, params object[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                text = text.Replace("#" + i, args[i].ToString());
+            }
+
+            return text;
+        }
+    }
+}
